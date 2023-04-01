e26d0cbe0b002518406bd48acb32ae6b65ee1be9
Rachit <bhojanirachit@gmail.com>
2023-04-01 19:53:16 +0530

    add chooseDevice code

---
+++ Chapter3_Introduction_to_CudaC/chooseDevice.cu
@@ -0,0 +1,13 @@
+#include <iostream>
+int main( void ) {
+ cudaDeviceProp prop;
+ int dev;
+ cudaGetDevice( &dev );
+ printf( "ID of current CUDA device: %d\n", dev );
+ memset( &prop, 0, sizeof( cudaDeviceProp ) );
+ prop.major = 1;
+ prop.minor = 3;
+ cudaChooseDevice( &dev, &prop );
+ printf( "ID of CUDA device closest to revision 1.3: %d\n", dev );
+ cudaSetDevice( dev );
+}

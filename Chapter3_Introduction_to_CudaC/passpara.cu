e32ea0e6727dd72732f2e084e80122ea02a12784
Rachit <bhojanirachit@gmail.com>
2023-04-01 19:49:02 +0530

    add passParameter code

--- Chapter3_Introduction_to_CudaC/passpara.cu
+++ Chapter3_Introduction_to_CudaC/passpara.cu
@@ -10,7 +10,7 @@ int main( void ) {
  cudaMalloc( (void**)&dev_c, sizeof(int) );
  add<<<1,1>>>( 2, 7, dev_c );
  cudaMemcpy( &c,dev_c,sizeof(int),cudaMemcpyDeviceToHost);
- printf( " 7 = %d\n", c );
+ printf( "2 + 7 = %d\n", c );
  cudaFree( dev_c );
  return 0;
 }
